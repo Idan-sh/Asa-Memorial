0a63ef21a2e18f4a76aa04acb72c5c3a1e2df3a1
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-09-23 08:25:56 +0300

    Added header title

--- frontend/src/cmps/header/Header.tsx
+++ frontend/src/cmps/header/Header.tsx
@@ -3,6 +3,11 @@ import profilePic from '/imgs/profile.jpeg';
 export default function Header() {
   return (
     <div className="header-container">
+      <div className="header-title-container">
+        <div className="main-title">אסא גיל - עד</div>
+        <div className="secondary-title">בן יהודית תמר ואמנון</div>
+      </div>
+      <div className="header-details-container"></div>
       <img className="profile-picture-img" src={profilePic} />
     </div>
   );

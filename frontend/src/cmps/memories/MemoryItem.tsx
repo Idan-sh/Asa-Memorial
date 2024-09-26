5fafd79b67b8537c8114e89501cb33ebd2931548
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-09-26 19:50:43 +0300

    Configured memory items styling, with adjustmnets to mobile screens.

--- frontend/src/cmps/memories/MemoryItem.tsx
+++ frontend/src/cmps/memories/MemoryItem.tsx
@@ -1,3 +1,15 @@
+import flameIcon from '/imgs/flame-icon.png';
+
 export default function MemoryItem() {
-  return <div className="memory-item-container">MemoryItem</div>;
+  return (
+    <div className="memory-item-container">
+      <img className="memory-item__icon" src={flameIcon} />
+      <div className="memory-item__name">שמעון שמעוני</div>
+      <div className="memory-item__relation">חבר</div>
+      <div className="memory-item__message">
+        מיאו מיאו מיאו מיאו מיאו מיאו מיאו מיאו
+      </div>
+      <button>לזיכרון המלא</button>
+    </div>
+  );
 }

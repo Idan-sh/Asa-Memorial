b17f20b957ae5db1527d5199c7c75c29b0eb91c0
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-10-01 21:19:30 +0300

    Added components for main content elements for better scalability and readabillity.

---
+++ frontend/src/cmps/main-content/RememberingSection.tsx
@@ -0,0 +1,16 @@
+export default function RememberingSection() {
+  return (
+    <div>
+      <h1>
+        זוכרים את אסא
+        <br />
+        בן יהודית תמר ואמנון
+      </h1>
+      <h2>
+        אתר זה נוצר לזכרו
+        <br />
+        האב האוהב, החבר הטוב ...
+      </h2>
+    </div>
+  );
+}

8666e5663d0fe2b1f9aa6943484da6058f1f7ae8
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-10-05 21:48:09 +0300

    Added fetch memories service to fetch memories from the backend to the frontend.

---
+++ frontend/src/services/fetch.memories.service.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { MemoryItemData } from "../models/MemoryItem.model";
+
+const fetchMemoriesUrl = import.meta.env.VITE_FETCH_MEMORIES_URL;
+
+interface fetchMemoriesResult {
+    success: boolean;
+    memories?: MemoryItemData[];
+}
+
+export async function fetchMemories(limit?: number) : Promise<fetchMemoriesResult>{
+    try {
+        const response = await axios.get<MemoryItemData[]>(fetchMemoriesUrl, {
+            params: limit,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        return {success: true, memories: response.data};
+    } catch (err) {
+        const errorMessage = axios.isAxiosError(err) && err.response
+        ? `Error: ${err.response.statusText}`
+        : 'An unknown error occurred.';
+        console.log("Error fetching memories... \n" + errorMessage);
+
+        return {success: false};
+    }
+}

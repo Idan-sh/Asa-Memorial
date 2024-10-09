3a4391936b1893d22f5b4a7ce2dafbeed1a9af03
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-10-09 11:02:10 +0300

    Added title to gallery, with some CSS adjusments.

--- frontend/src/cmps/gallery/Gallery.tsx
+++ frontend/src/cmps/gallery/Gallery.tsx
@@ -3,12 +3,15 @@ import GalleryAlbum from './GalleryAlbum';
 export default function Gallery() {
   return (
     <div className="gallery-content-container">
-      <GalleryAlbum title="Some Album" id="1" />
-      <GalleryAlbum title="Some Album" id="2" />
-      <GalleryAlbum title="Some Album" id="3" />
-      <GalleryAlbum title="Some Album" id="4" />
-      <GalleryAlbum title="Some Album" id="5" />
-      <GalleryAlbum title="Some Album" id="6" />
+      <h2>גלריה</h2>
+      <div className="gallery-content-albums-container">
+        <GalleryAlbum title="Some Album" id="1" />
+        <GalleryAlbum title="Some Album" id="2" />
+        <GalleryAlbum title="Some Album" id="3" />
+        <GalleryAlbum title="Some Album" id="4" />
+        <GalleryAlbum title="Some Album" id="5" />
+        <GalleryAlbum title="Some Album" id="6" />
+      </div>
     </div>
   );
 }

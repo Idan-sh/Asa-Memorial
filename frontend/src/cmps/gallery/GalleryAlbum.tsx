2aa64d66eebd4733c6fb5619feb5fb9e3c75429e
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-10-12 08:51:01 +0300

    Added skeleton loader to gallery albums.

--- frontend/src/cmps/gallery/GalleryAlbum.tsx
+++ frontend/src/cmps/gallery/GalleryAlbum.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import defaultImg from '/imgs/defaultAlbumImg.jpeg';
+import { useState } from 'react';
 
 interface GalleryAlbumProps {
   title: string;
@@ -8,12 +9,23 @@ interface GalleryAlbumProps {
 
 export default function GalleryAlbum({ title, id }: GalleryAlbumProps) {
   const navigate = useNavigate();
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const goToAlbum = () => navigate('/album/' + id);
 
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
+
   return (
     <div className="gallery-album-container" onClick={goToAlbum}>
-      <img src={defaultImg} />
+      {!isImageLoaded && <div className="image-placeholder"></div>}
+      <img
+        src={defaultImg}
+        onLoad={handleImageLoad}
+        alt={title}
+        style={{ display: isImageLoaded ? 'block' : 'none' }}
+      />
       <h1>{title}</h1>
     </div>
   );

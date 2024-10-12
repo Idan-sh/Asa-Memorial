b54f36316bc740a772b0f08c206f56bff1ccdde2
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-10-12 11:06:28 +0300

    Added light gallery to Album component to display the album's images.

--- frontend/src/cmps/gallery/Album.tsx
+++ frontend/src/cmps/gallery/Album.tsx
@@ -1,4 +1,12 @@
 import { useParams } from 'react-router-dom';
+import LightGallery from 'lightgallery/react';
+
+import 'lightgallery/scss/lightgallery.scss';
+import 'lightgallery/scss/lg-zoom.scss';
+import 'lightgallery/scss/lg-video.scss';
+
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
 
 export default function Album() {
   const { albumId } = useParams();
@@ -6,7 +14,21 @@ export default function Album() {
   return (
     <div className="album-container">
       <h2>Album {albumId}</h2>
-      <div></div>
+      <LightGallery
+        speed={500}
+        plugins={[lgThumbnail, lgZoom]}
+        mode="lg-fade"
+        zoomFromOrigin={false}
+        download={false}
+        thumbnail={false}
+      >
+        <a href="/imgs/gallery/gallery-bike1.JPG" data-lg-size="1600-2400">
+          <img alt="img1" src="/imgs/gallery/gallery-bike1.JPG" />
+        </a>
+        <a href="/imgs/gallery/gallery-bike2.JPG">
+          <img alt="img2" src="/imgs/gallery/gallery-bike2.JPG" />
+        </a>
+      </LightGallery>
     </div>
   );
 }

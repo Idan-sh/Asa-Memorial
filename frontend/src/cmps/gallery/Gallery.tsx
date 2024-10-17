6d15ae94e3c958cc5b5ac3b72ce3ce7d46e534bd
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-10-17 11:10:45 +0300

    Added more albums

--- frontend/src/cmps/gallery/Gallery.tsx
+++ frontend/src/cmps/gallery/Gallery.tsx
@@ -5,6 +5,14 @@ export default function Gallery() {
     <div className="gallery-content-container">
       <h2>גלריה</h2>
       <div className="gallery-content-albums-container">
+        <GalleryAlbum title="ילדות ונעורים" folderName="childhood" />
+        <GalleryAlbum title="מסביב לעולם" folderName="aroundTheWorld" />
+        <GalleryAlbum
+          title="נעורים בירושלים: עלייה והתבגרות"
+          folderName="growingUpInJerusalem"
+        />
+        <GalleryAlbum title="שירות בצה״ל" folderName="Army" />
+        <GalleryAlbum title="שירות ביס״מ ירושלים" folderName="Police" />
         <GalleryAlbum title="אופניים" folderName="bike" />
         <GalleryAlbum title="משפחה" folderName="family" />
       </div>

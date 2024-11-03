62c50b31ec57493219a6ae2519dc82ef702b3a6d
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-11-03 11:06:54 +0200

    Removed animation from go back button component.

--- frontend/src/cmps/global/GoBackButton.tsx
+++ frontend/src/cmps/global/GoBackButton.tsx
@@ -1,44 +1,14 @@
-import Lottie, { LottieRefCurrentProps } from 'lottie-react';
-import backButtonAnimation from '../../assets/animations/back-button-animation.json';
-import { useRef } from 'react';
-
 interface GoBackButtonProps {
   onGoBackClick: () => void;
 }
 export default function GoBackButton({ onGoBackClick }: GoBackButtonProps) {
-  const backButtonLottieRef = useRef<LottieRefCurrentProps>(null);
-
   const onClick = () => {
-    backButtonLottieRef.current?.playSegments([80, 110], true);
-    setTimeout(onGoBackClick, 500);
-  };
-
-  const handleMouseEnter = () => {
-    backButtonLottieRef.current?.playSegments([30, 60], true);
-  };
-
-  const handleMouseLeave = () => {
-    backButtonLottieRef.current?.playSegments([130, 200], true);
+    onGoBackClick();
   };
 
   return (
-    <div
-      className="back-button-container"
-      onClick={onClick}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <Lottie
-        lottieRef={backButtonLottieRef}
-        animationData={backButtonAnimation}
-        loop={false}
-        autoplay={false}
-        style={{
-          width: '140px',
-          height: 'auto',
-          cursor: 'pointer',
-        }}
-      />
+    <div className="back-button-container">
+      <button onClick={onClick}>חזור</button>
     </div>
   );
 }

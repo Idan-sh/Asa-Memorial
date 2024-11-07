749416e4ce98df5d9fac0fec584214a48b435012
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-11-07 18:23:39 +0200

    Added instagram link to real instagram, instead of the old instagram widget.

--- frontend/src/cmps/content/instagram-content/InstagramContent.tsx
+++ frontend/src/cmps/content/instagram-content/InstagramContent.tsx
@@ -1,17 +1,15 @@
 export default function InstagramContent() {
+  const onInstagramClick = () => {
+    window.open('https://www.instagram.com/asagilad/', '_blank');
+  };
+
   return (
     <div className="instagram-content-container">
       <h2>רגעים של אסא</h2>
-      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-      <iframe
-        src="//lightwidget.com/widgets/84377f181f38528da163a9039797e949.html"
-        className="lightwidget-widget"
-        style={{
-          width: '100%',
-          height: '400px',
-          border: '0',
-        }}
-      ></iframe>
+      <div className="instagram-content-link" onClick={onInstagramClick}>
+        <img src="/icons/instagram.png" width="50px" alt="Instagram Icon" />
+        <p>asagilad</p>
+      </div>
     </div>
   );
 }
